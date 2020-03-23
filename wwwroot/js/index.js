906f98a9019640618054b7e8c828a9b696c9f977
Billy Shelton <lakay57@hotmail.com>
2020-03-22 21:39:22 -0500

    Starting JQuery

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,1 +1,7 @@
-﻿
+﻿var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
