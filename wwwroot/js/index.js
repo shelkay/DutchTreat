8e04f073801e3d399bf0279af3b61321cad51a92
Billy Shelton <lakay57@hotmail.com>
2020-03-24 21:24:25 -0500

    Javascript module

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,7 +1,24 @@
-﻿var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+﻿$(document).ready(function () {
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function () {
-    console.log("Buying Item");
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    //var listItems = productInfo.item[0].children;
+
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
+
 });
